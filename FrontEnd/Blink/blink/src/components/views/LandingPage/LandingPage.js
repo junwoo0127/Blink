46451d07bc7e66651f6f427291968a43d708c0e2
minjae <minjae77777@naver.com>
2022-07-27 11:02:29 +0900

    [FE]Feat/Login

---
+++ FrontEnd/Blink/blink/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function LandingPage(props) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get("/api/hello");
+  }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.navigate("/login");
+      } else {
+        alert("로그아웃실패!");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
+    </div>
+  );
+}
+
+export default LandingPage;
