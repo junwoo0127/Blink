4f2fb79b3d0cf020ea4ee7551ef825bee96ddc22
minjae <minjae77777@naver.com>
2022-08-03 14:35:04 +0900

    [FE/Update/Login]

--- FrontEnd/src/components/LoginPage/LoginPage.js
+++ FrontEnd/src/components/LoginPage/LoginPage.js
@@ -3,8 +3,13 @@ import { useDispatch } from "react-redux";
 import { loginUser } from "../../_actions/user_action";
 // import { useParams, useLocation, useNavigate } from "react-router-dom";
 
+// import LandingPage from "./components/LandingPage/LandingPage";
+
 import { alpha, styled } from "@mui/material/styles";
 import logo_ani from "../../assets/logo_ani.gif";
+
+import MemberPage from "../Common/MemberPage";
+import Footer from "../Common/Footer";
 
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
@@ -15,15 +20,15 @@ import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 // import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
-// 바탕  #141414v
+// 검  #141414v
 // 흰글씨 #F3F3F3
 // 회글씨#686868
 
-const ColorButton = styled(Button)(({ theme }) => ({
+const ButtonCo = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText("#A6095D"),
   lineHeight: "44px",
   borderRadius: "30px",
@@ -34,18 +39,30 @@ const ColorButton = styled(Button)(({ theme }) => ({
   },
 }));
 
-const TextFieldLogin = styled(TextField)({
-  textAlign: "center !important",
+// style={{ margin: "auto 5px auto 0px !important" }}
 
+const Grid1 = styled(Grid)({
+  "& .MuiGrid-root": {
+    margin: "auto 10px auto 0px ",
+  },
+});
+
+const TextFieldLogin = styled(TextField)({
+  "& .MuiFormLabel-root": {
+    color: "#141414", // 클릭전 텍스트
+    left: "0.5%",
+    top: "10%",
+    // textAlign: "center !important",
+  },
   "& label.Mui-focused": {
-    color: "#141414", // 위로 올라갈때
+    color: "#ffffff", // 위로 올라간텍스트
   },
   "& .MuiInput-underline:after": {
     borderBottomColor: "white",
   },
   "& .MuiOutlinedInput-root": {
     "& input": {
-      // 안에 텍스트
+      // 안에 작성할 텍스트
       color: "#141414",
       fontSize: "21px",
     },
@@ -98,90 +115,164 @@ function LoginPage(props) {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 12,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <img
-          alt="logo_ani"
-          src={logo_ani}
-          style={{
-            width: "100px",
+    <div>
+      <MemberPage></MemberPage>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 5,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-          sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
-        />
-
-        <form
-          style={{ display: "flex", flexDirection: "column" }}
-          onSubmit={onSubmitHandler}
+          style={{
+            justifyContent: "center",
+            height: "100vh",
+          }}
         >
-          <Box
-            component="form"
-            // onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+          <img
+            alt="logo_ani"
+            src={logo_ani}
+            style={{
+              maxWidth: "200px",
+              width: "40vw",
+              borderRadius: "50%",
+            }}
+          />
+
+          <form
+            style={{ display: "flex", flexDirection: "column" }}
+            onSubmit={onSubmitHandler}
           >
-            <TextFieldLogin
-              margin="normal"
-              fullWidth
-              value={Email}
-              onChange={onEmailHandler}
-              id="email"
-              label="아이디"
-              name="email"
-              type="email"
-              autoComplete="email"
-              autoFocus
-              // sx={{ mx: "auto" }}
-            />
-            <TextFieldLogin
-              margin="normal"
-              fullWidth
-              value={Password}
-              onChange={onPasswordHandler}
-              id="password"
-              name="password"
-              label="비밀번호"
-              type="password"
-              autoComplete="current-password"
-            />
-
-            {/* 개구림 저장ui 지우고 싶다 */}
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-              style={{ marginLeft: "10px" }}
-            />
-            <ColorButton
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mb: 3 }}
+            <Box
+              component="form"
+              // onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
             >
-              <b>로그인</b>
-            </ColorButton>
-            <Typography align="center">
-              <Link href="#" underline="hover" variant="h6" color="inherit">
-                회원이 아니신가요? <b>회원가입</b>
-              </Link>
-              <br></br>
-              <Link
-                href="#"
-                underline="hover"
-                variant="subtitle2"
-                color="inherit"
-              >
-                아이디 혹은 비밀번호 찾기
-              </Link>
-            </Typography>
-
-            {/*  나란히 링크 만들때 참고 
+              {1 === 1 ? (
+                <div>
+                  <TextFieldLogin
+                    margin="normal"
+                    fullWidth
+                    value={Email}
+                    onChange={onEmailHandler}
+                    id="email"
+                    label="아이디"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    autoFocus
+                    // sx={{ mx: "auto" }}
+                  />
+                  <TextFieldLogin
+                    margin="normal"
+                    fullWidth
+                    value={Password}
+                    onChange={onPasswordHandler}
+                    id="password"
+                    name="password"
+                    label="비밀번호"
+                    type="password"
+                    autoComplete="current-password"
+                  />
+                  <Grid1 container>
+                    <Grid item xs>
+                      <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="로그인 정보 저장"
+                        style={{ marginLeft: "5px" }}
+                      />
+                    </Grid>
+                    <Grid item>
+                      <Link
+                        href="#"
+                        underline="hover"
+                        variant="subtitle2"
+                        color="inherit"
+                      >
+                        도움이 필요하신가요?
+                      </Link>
+                    </Grid>
+                  </Grid1>
+                  <ButtonCo
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mb: 3 }}
+                  >
+                    <b>로그인</b>
+                  </ButtonCo>
+                  <Typography align="center">
+                    <Link
+                      href="#"
+                      underline="hover"
+                      variant="h6"
+                      color="inherit"
+                    >
+                      회원이 아니신가요? <b>지금 가입하세요</b>
+                    </Link>
+                    <br></br>
+                    <Link
+                      href="#"
+                      underline="hover"
+                      variant="subtitle2"
+                      color="inherit"
+                    >
+                      아이디 혹은 비밀번호 찾기
+                    </Link>
+                  </Typography>
+                </div>
+              ) : (
+                <div>
+                  <Typography
+                    component="h3"
+                    variant="h5"
+                    style={{ textAlign: "center", margin: "30px auto 0px" }}
+                  >
+                    안녕하세요 ~~ 홍길동님
+                  </Typography>
+                  <TextFieldLogin
+                    margin="normal"
+                    fullWidth
+                    // value={}
+                    // onChange={onEmailHandler}
+                    id="link"
+                    label="참가링크"
+                    name="link"
+                    type="link"
+                    autoComplete="link"
+                    autoFocus
+                  />
+                  <ButtonCo
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mb: 3 }}
+                  >
+                    <b>참가</b>
+                  </ButtonCo>{" "}
+                  <ButtonCo
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mb: 3 }}
+                  >
+                    <b>방 생성하기</b>
+                  </ButtonCo>
+                </div>
+              )}
+            </Box>
+          </form>
+        </Box>
+      </Container>
+      <Footer sx={{ mt: 5, mb: 3 }} />;
+    </div>
+  );
+}
+{
+  /*  나란히 링크 만들때 참고 
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
@@ -193,14 +284,8 @@ function LoginPage(props) {
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
-            </Grid> */}
-          </Box>
-        </form>
-      </Box>
-    </Container>
-  );
+            </Grid> */
 }
-
 export default LoginPage;
 // withRouter 잘모르겠다
 // 원래는 39번줄 props.history.push 에러 문제로 withRouter강의에서씀
