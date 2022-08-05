8a39bdda2da7a7f6ec620d0df9a5af2662a6155f
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2022-08-05 14:58:20 +0900

    [BE]feature/player : player Controller, Enter Service

--- BackEnd/src/main/java/com/ssafy/api/service/PlayerService.java
+++ BackEnd/src/main/java/com/ssafy/api/service/PlayerService.java
@@ -1,9 +1,11 @@
 package com.ssafy.api.service;
 
+import com.ssafy.api.request.PlayerEnterPostReq;
 import com.ssafy.db.entity.Player;
 
 public interface PlayerService {
-	Player enterPlayer(Player player);
+	Player findPlayerByPlayerSeq(Long id);
+	Player enterPlayer(PlayerEnterPostReq player);
 	Player updatePlayer(Player player);
 	void liarUpdate(Player player);
 }
