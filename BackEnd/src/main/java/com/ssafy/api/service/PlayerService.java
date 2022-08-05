8676e8914aa90c4445d5d4900dd59971ad94ed28
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2022-08-05 13:36:36 +0900

    [BE]Feature/Room : request class push

---
+++ BackEnd/src/main/java/com/ssafy/api/service/PlayerService.java
@@ -0,0 +1,8 @@
+package com.ssafy.api.service;
+
+import com.ssafy.db.entity.Player;
+
+public interface PlayerService {
+	Player enterPlayer(Player player);
+	Player updatePlayer(Player player);
+}
