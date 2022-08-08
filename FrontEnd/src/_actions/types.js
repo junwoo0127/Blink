1cb73f30d138de186fb4a6b88812d65849e6aed9
minjae <minjae77777@naver.com>
2022-08-08 09:59:44 +0900

    [FE]복구

--- FrontEnd/src/_actions/types.js
+++
@@ -1,5 +0,0 @@
-export const LOGIN_USER = "login_user";
-export const REGISTER_USER = "register_user";
-export const AUTH_USER = "auth_user";
-export const GET_USER = "get_user";
-export const CHECK_ID = "check_id";
